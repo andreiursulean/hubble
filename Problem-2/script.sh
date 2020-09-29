259a959f111d12ff6f825930b3ab644852c9219d
Andrei Ursulean <andreiursulean98@gmail.com>
2020-09-29 21:39:55 +0300

    counted participants per country and written to file

--- Problem-2/script.sh
+++ Problem-2/script.sh
@@ -1,6 +1,17 @@
+declare -A number
+
+echo "Parsing File..."
 while read line;
 do 
-    echo $line;
+    # echo $line;
     country="$(cut -d',' -f3 <<< $line)"
-    echo $country
+    number[$country]=$(( number[$country] + 1))
 done < './teams.csv'
+
+echo "Writing results..."
+for K in "${!number[@]}"; 
+do 
+    echo ${K},${number[$K]} >> summary.csv
+done
+
+echo "Done"
